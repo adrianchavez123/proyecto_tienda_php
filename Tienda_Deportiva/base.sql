25cb351b5ece466e56caa6b0f77d624c47e7f612
adrianchavez123 <adrianchavez065@gmail.com>
2015-03-25 10:30:03 -0600

    base ahora si

--- Tienda_Deportiva/base.sql
+++ Tienda_Deportiva/base.sql
@@ -39,8 +39,79 @@ create table productos
 	demanda_diaria int not null,
 	tiempo_de_entrega varchar(30)not null,
 	cantidad_productos_minimos int not null,
+	punto_reorden varchar(50) not null,
 	activo varchar(50) not null,
 	unique key (codigo),
 	primary key (id)
 ) engine= Innodb;
 
+create table compras
+(
+	numero_orden int not null auto_increment,
+	proveedor_id int not null,
+	fecha_pedido date not null,
+	foreign key (proveedor_id) references proveedores(id)
+	on update cascade
+	on delete restrict,
+	primary key (numero_orden)
+
+) engine = Innodb;
+
+
+create table detalle_compra
+(
+	id int not null auto_increment,
+	numero_orden int not null,
+	producto_id int not null,
+	foreign key (producto_id) references productos(id)
+	on update cascade
+	on delete restrict,
+	primary key (id)
+
+) engine = Innodb;
+
+
+create table recepcion
+(
+	numero_recepcion int not null auto_increment,
+	proveedor_id int not null,
+	compra_id int not null,
+	fecha_recepcion date not null,
+	foreign key (proveedor_id) references proveedores(id)
+	on delete restrict
+	on update cascade,
+	foreign key (compra_id) references proveedores(id)
+	on delete restrict
+	on update cascade,
+	primary key (numero_recepcion)
+
+)	engine = Innodb;
+
+create table ventas
+(
+	numero_orden int not null,
+	nombre_razon_social varchar(200) not null,
+	fecha_pedido date not null,
+	usuario_id int not null,
+	rfc varchar(200) not null,
+	foreign key (usuario_id) references usuarios(id)
+	on update cascade
+	on delete restrict,
+	primary key (rfc)
+
+) engine = Innodb;
+
+
+create table detalle_venta
+(
+	id int not null auto_increment,
+	numero_orden int not null,
+	producto_id int not null,
+	foreign key (producto_id) references productos(id)
+	on update cascade
+	on delete restrict,
+	primary key (id)
+
+) engine = Innodb;
+
+insert into usuarios values(null,'admin','admin','admin','admin',md5('admin'));
